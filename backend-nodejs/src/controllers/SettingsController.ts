6fece8511b972e93d988596ca9c788b2f913bfa0
ygor-salles <ygorsallesaniceto@gmail.com>
2021-04-21 16:44:25 -0300

    Subindo metade da segunda aula

--- backend-nodejs/src/controllers/SettingsController.ts
+++ backend-nodejs/src/controllers/SettingsController.ts
@@ -1,17 +1,21 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "../repositories/SettingsRepository";
+import { SettingsService } from '../services/SetttingsService';
 
 class SettingsController {
     async create(request: Request, response: Response) {
         const { chat, username } = request.body
+        
+        const settingsService = new SettingsService();
 
-        const settingsRepository = getCustomRepository(SettingsRepository);
-        const settings = settingsRepository.create({ chat, username });
-
-        await settingsRepository.save(settings);
-        return response.json(settings);
+        try {
+            const settings = await settingsService.create({ chat, username })
+            return response.json(settings);
+        } catch (error) {
+            return response.status(400).json({
+                message: error.message
+            });
+        }
     }
 }
 
-export { SettingsController }
+export { SettingsController };
